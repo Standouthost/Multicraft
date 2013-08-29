3bc52fd3acf48f083844a52779d4df49d062984b
Jmainguy <jon@soh.re>
2013-08-28 21:13:06 -0400

    Mumbles

---
+++ jar/mumble.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+SERVER_DIR="$1"
+SERVER_PORT="$2"
+MAX_PLAYERS="$3"
+SERVER_IP="$4"
+PASSWORD="$5"
+
+#Run server as user
+KILL=`/usr/sbin/lsof -t -i @"$SERVER_IP":"$SERVER_PORT"`
+kill -9 "$KILL"
+
+sed "s/port.*/port=$SERVER_PORT/g" $SERVER_DIR/Mumble/murmur.ini > $SERVER_DIR/Mumble/new.xml
+
+mv $SERVER_DIR/Mumble/new.xml $SERVER_DIR/Mumble/murmur.ini
+
+sed "s/.*host=.*/host=$SERVER_IP/g" $SERVER_DIR/Mumble/murmur.ini > $SERVER_DIR/Mumble/new.xml
+
+mv $SERVER_DIR/Mumble/new.xml $SERVER_DIR/Mumble/murmur.ini
+
+sed "s/users.*/users=$MAX_PLAYERS/g" $SERVER_DIR/Mumble/murmur.ini > $SERVER_DIR/Mumble/new.xml
+
+mv $SERVER_DIR/Mumble/new.xml $SERVER_DIR/Mumble/murmur.ini
+
+$SERVER_DIR/Mumble/murmur.x86 -ini $SERVER_DIR/Mumble/murmur.ini -supw $PASSWORD
+
+$SERVER_DIR/Mumble/murmur.x86
+echo "Server is running, there is no console"
+echo "But trust me, it is running"
+tail -f $SERVER_DIR/Mumble/murmur.log
