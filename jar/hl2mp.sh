dbe82944efa5ef0c7e98bf620d350236d3dd4291
Jmainguy <jon@soh.re>
2013-08-29 10:55:49 -0400

    Moved Half Life 2 to steamcmd

--- jar/hl2mp.sh
+++ jar/hl2mp.sh
@@ -3,14 +3,20 @@ PLAYERS="$1"
 WORLD="$2"
 PORT="$3"
 SERVER_DIR="$4"
-cd "$SERVER_DIR"/Steam/
-"$SERVER_DIR"/Steam/steam -command update -game hl2mp -dir steamserver
-"$SERVER_DIR"/Steam/steam -command update -game hl2mp -dir steamserver
-if [ $WORLD == "world" ]
-then
-cd "$SERVER_DIR"/Steam/steamserver/orangebox
-"$SERVER_DIR"/Steam/steamserver/orangebox/srcds_run -game hl2mp -autoupdate -tickrate 66 -maxplayers $PLAYERS +map dm_lockdown -port $PORT
+if [ $WORLD == 'world' ]; then
+	cd "$SERVER_DIR"/Steam/
+	"$SERVER_DIR"/Steam/steamcmd.sh +runscript hl2mp.txt
+fi
+if [ -d "$SERVER_DIR"/.steam ]; then
+	echo "cool"
 else
-cd "$SERVER_DIR"/Steam/steamserver/orangebox
-"$SERVER_DIR"/Steam/steamserver/orangebox/srcds_run -game hl2mp -autoupdate -tickrate 66 -maxplayers $PLAYERS +map $WORLD -port $PORT
+	mkdir -p "$SERVER_DIR"/.steam/sdk32
+	rsync -avz "$SERVER_DIR"/Steam/linux32/ "$SERVER_DIR"/.steam/sdk32
+fi
+if [ $WORLD == "world" ]; then
+	cd "$SERVER_DIR"/Steam/hl2mp
+	"$SERVER_DIR"/Steam/hl2mp/srcds_run -game hl2mp -tickrate 66 -maxplayers $PLAYERS +map dm_lockdown -port $PORT
+else
+	cd "$SERVER_DIR"/Steam/tf2
+	"$SERVER_DIR"/Steam/hl2mp/srcds_run -game hl2mp -tickrate 66 -maxplayers $PLAYERS +map $WORLD -port $PORT
 fi
