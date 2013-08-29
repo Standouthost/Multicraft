26c9b0446c5ec5996b1e9e21da0e437593b9c77d
Jmainguy <jon@soh.re>
2013-08-28 22:12:36 -0400

    Improved sed

--- jar/BungeeCord.sh
+++ jar/BungeeCord.sh
@@ -5,13 +5,8 @@ MAX_PLAYERS="$3"
 SERVER_IP="$4"
 PASSWORD="$5"
 
-sed "s/host:.*/host: $SERVER_IP\:$SERVER_PORT/g" $SERVER_DIR/config.yml > $SERVER_DIR/config.yml.tmp
-
-mv $SERVER_DIR/config.yml.tmp $SERVER_DIR/config.yml
-
-sed "s/max_players.*/max_players: $MAX_PLAYERS/g" $SERVER_DIR/config.yml > $SERVER_DIR/config.yml.tmp
-
-mv $SERVER_DIR/config.yml.tmp $SERVER_DIR/config.yml
+sed -i "s/host:.*/host: $SERVER_IP\:$SERVER_PORT/g" $SERVER_DIR/config.yml
+sed -i "s/max_players.*/max_players: $MAX_PLAYERS/g" $SERVER_DIR/config.yml
 
 cd $SERVER_DIR
 java -jar $SERVER_DIR/BungeeCord.jar
