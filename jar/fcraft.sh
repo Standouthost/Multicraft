ce8efece389d285ded18aa36026d8deab188703e
Jmainguy <jon@soh.re>
2013-08-28 22:25:10 -0400

    fCraft

---
+++ jar/fcraft.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SERVER_DIR="$1"
+SERVER_PORT="$2"
+MAX_PLAYERS="$3"
+SERVER_IP="78.46.213.213"
+
+#Run server as user
+KILL=`/usr/sbin/lsof -t -i @"$SERVER_IP":"$SERVER_PORT"`
+kill -9 "$KILL"
+
+sed 's/.*"MaxPlayers".*/    <ConfigKey key="MaxPlayers" value="'$MAX_PLAYERS'" default="20" \/>/g' $SERVER_DIR/classic/config.xml > $SERVER_DIR/classic/new.xml
+
+mv $SERVER_DIR/classic/new.xml $SERVER_DIR/classic/config.xml
+
+sed 's/.*"Port".*/    <ConfigKey key="Port" value="'$SERVER_PORT'" default="25565" \/>/g' $SERVER_DIR/classic/config.xml > $SERVER_DIR/classic/new.xml
+
+mv $SERVER_DIR/classic/new.xml $SERVER_DIR/classic/config.xml
+
+export PATH=$PATH:/opt/mono/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/mono/lib
+/opt/mono/bin/mozroots --import --ask-remove 
+
+/opt/mono/bin/mono --gc=sgen $SERVER_DIR/classic/ServerCLI.exe
