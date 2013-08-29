3279ec45059a0772964aafeaac74bceaaac2de37
Jmainguy <jon@soh.re>
2013-08-28 21:18:15 -0400

    800Craft

---
+++ jar/800craft.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SERVER_DIR="$1"
+SERVER_PORT="$2"
+MAX_PLAYERS="$3"
+SERVER_IP="$4"
+
+#Run server as user
+KILL=`/usr/sbin/lsof -t -i @"$SERVER_IP":"$SERVER_PORT"`
+kill -9 "$KILL"
+
+sed 's/.*"MaxPlayers".*/    <ConfigKey key="MaxPlayers" value="'$MAX_PLAYERS'" default="20" \/>/g' $SERVER_DIR/800craft/config.xml > $SERVER_DIR/800craft/new.xml
+
+mv $SERVER_DIR/800craft/new.xml $SERVER_DIR/800craft/config.xml
+
+sed 's/.*"Port".*/    <ConfigKey key="Port" value="'$SERVER_PORT'" default="25565" \/>/g' $SERVER_DIR/800craft/config.xml > $SERVER_DIR/800craft/new.xml
+
+mv $SERVER_DIR/800craft/new.xml $SERVER_DIR/800craft/config.xml
+
+export PATH=$PATH:/opt/mono/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/opt/mono/bin
+mozroots --import --ask-remove 
+
+/opt/mono/bin/mono --gc=sgen $SERVER_DIR/800craft/ServerCLI.exe
