44f0e99d63326f72194c587ff447841cfd65629e
Jmainguy <jon@soh.re>
2013-09-06 11:30:43 -0400

    improved script for initial installs

--- jar/garrysmod.sh
+++ jar/garrysmod.sh
@@ -3,23 +3,22 @@ PLAYERS="$1"
 WORLD="$2"
 PORT="$3"
 SERVER_DIR="$4"
-if [ $WORLD == 'update' ]; then
+if [ $WORLD == 'update' -o $WORLD == 'world' ]; then
 	cd "$SERVER_DIR"/Steam/
 	"$SERVER_DIR"/Steam/steamcmd.sh +runscript gmod.txt
 fi
-if [ `ls $SERVER_DIR/.steam | wc -l` -gt 0 ];
-then echo "cool";
+if [ `ls $SERVER_DIR/.steam | wc -l` -gt 0 ]; then
+	echo "cool"
 else
-mkdir "$SERVER_DIR"/.steam;
-mkdir "$SERVER_DIR"/.steam/sdk32;
-cp "$SERVER_DIR"/Steam/linux32/libsteam.so "$SERVER_DIR"/.steam/sdk32/
-cp "$SERVER_DIR"/Steam/linux32/libstdc++.so.6 "$SERVER_DIR"/Steam/gmod/bin/
+	mkdir "$SERVER_DIR"/.steam;
+	mkdir "$SERVER_DIR"/.steam/sdk32;
+	cp "$SERVER_DIR"/Steam/linux32/libsteam.so "$SERVER_DIR"/.steam/sdk32/
+	cp "$SERVER_DIR"/Steam/linux32/libstdc++.so.6 "$SERVER_DIR"/Steam/gmod/bin/
 fi
-if [ $WORLD == "world" -o $WORLD == "update" ]
-then
-cd "$SERVER_DIR"/Steam/gmod
-"$SERVER_DIR"/Steam/gmod/srcds_run -game garrysmod -autoupdate -tickrate 66 -maxplayers $PLAYERS +map gm_construct -port $PORT
+if [ $WORLD == "world" -o $WORLD == "update" ]; then
+	cd "$SERVER_DIR"/Steam/gmod
+	"$SERVER_DIR"/Steam/gmod/srcds_run -game garrysmod -autoupdate -tickrate 66 -maxplayers $PLAYERS +map gm_construct -port $PORT
 else
-cd "$SERVER_DIR"/Steam/gmod
-"$SERVER_DIR"/Steam/gmod/srcds_run -game garrysmod -autoupdate -tickrate 66 -maxplayers $PLAYERS +map $WORLD -port $PORT
+	cd "$SERVER_DIR"/Steam/gmod
+	"$SERVER_DIR"/Steam/gmod/srcds_run -game garrysmod -autoupdate -tickrate 66 -maxplayers $PLAYERS +map $WORLD -port $PORT
 fi
