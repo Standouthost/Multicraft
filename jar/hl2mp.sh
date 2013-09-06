8ac09aeda65090e5dbd33c5cb509411819c6cc36
Jmainguy <jon@soh.re>
2013-09-06 12:01:03 -0400

    added update as a choice to update mods

--- jar/hl2mp.sh
+++ jar/hl2mp.sh
@@ -3,20 +3,18 @@ PLAYERS="$1"
 WORLD="$2"
 PORT="$3"
 SERVER_DIR="$4"
-if [ $WORLD == 'world' ]; then
-	cd "$SERVER_DIR"/Steam/
-	"$SERVER_DIR"/Steam/steamcmd.sh +runscript hl2mp.txt
-fi
 if [ -d "$SERVER_DIR"/.steam ]; then
 	echo "cool"
 else
 	mkdir -p "$SERVER_DIR"/.steam/sdk32
 	rsync -avz "$SERVER_DIR"/Steam/linux32/ "$SERVER_DIR"/.steam/sdk32
 fi
-if [ $WORLD == "world" ]; then
+if [ $WORLD == 'world' -o $WORLD == 'update' ]; then
+	cd "$SERVER_DIR"/Steam/
+	"$SERVER_DIR"/Steam/steamcmd.sh +runscript hl2mp.txt
 	cd "$SERVER_DIR"/Steam/hl2mp
 	"$SERVER_DIR"/Steam/hl2mp/srcds_run -game hl2mp -tickrate 66 -maxplayers $PLAYERS +map dm_lockdown -port $PORT
 else
-	cd "$SERVER_DIR"/Steam/tf2
+	cd "$SERVER_DIR"/Steam/hl2mp
 	"$SERVER_DIR"/Steam/hl2mp/srcds_run -game hl2mp -tickrate 66 -maxplayers $PLAYERS +map $WORLD -port $PORT
 fi
--- jar/tf2.sh
+++ jar/tf2.sh
@@ -3,17 +3,15 @@ PLAYERS="$1"
 WORLD="$2"
 PORT="$3"
 SERVER_DIR="$4"
-if [ $WORLD == 'world' ]; then
-	cd "$SERVER_DIR"/Steam/
-	"$SERVER_DIR"/Steam/steamcmd.sh +runscript tf2.txt
-fi
 if [ -d "$SERVER_DIR"/.steam ]; then
 	echo "cool"
 else
 	mkdir -p "$SERVER_DIR"/.steam/sdk32
 	rsync -avz "$SERVER_DIR"/Steam/linux32/ "$SERVER_DIR"/.steam/sdk32
 fi
-if [ $WORLD == "world" ]; then
+if [ $WORLD == 'world' -o $WORLD == 'update' ]; then
+	cd "$SERVER_DIR"/Steam/
+	"$SERVER_DIR"/Steam/steamcmd.sh +runscript tf2.txt
 	cd "$SERVER_DIR"/Steam/tf2
 	"$SERVER_DIR"/Steam/tf2/srcds_run -game tf -tickrate 66 -maxplayers $PLAYERS +map cp_gorge -port $PORT
 else
