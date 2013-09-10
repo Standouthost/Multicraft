6bba29de6d5440562d128b76c95e7a323319e33f
Jmainguy <jon@soh.re>
2013-09-10 08:21:40 -0400

    improved creation process

--- jar/garrysmod.sh
+++ jar/garrysmod.sh
@@ -3,17 +3,16 @@ PLAYERS="$1"
 WORLD="$2"
 PORT="$3"
 SERVER_DIR="$4"
-if [ `ls $SERVER_DIR/.steam | wc -l` -gt 0 ]; then
+if [ -d $SERVER_DIR/.steam ]; then
 	echo "cool"
 else
-	mkdir "$SERVER_DIR"/.steam;
-	mkdir "$SERVER_DIR"/.steam/sdk32;
-	cp "$SERVER_DIR"/Steam/linux32/libsteam.so "$SERVER_DIR"/.steam/sdk32/
-	cp "$SERVER_DIR"/Steam/linux32/libstdc++.so.6 "$SERVER_DIR"/Steam/gmod/bin/
+	mkdir -p "$SERVER_DIR"/.steam/sdk32;
 fi
 if [ $WORLD == 'update' -o $WORLD == 'world' ]; then
 	cd "$SERVER_DIR"/Steam/
 	"$SERVER_DIR"/Steam/steamcmd.sh +runscript gmod.txt
+	cp "$SERVER_DIR"/Steam/linux32/libsteam.so "$SERVER_DIR"/.steam/sdk32/
+	cp "$SERVER_DIR"/Steam/linux32/libstdc++.so.6 "$SERVER_DIR"/Steam/gmod/bin/
 	cd "$SERVER_DIR"/Steam/gmod
 	"$SERVER_DIR"/Steam/gmod/srcds_run -game garrysmod -autoupdate -tickrate 66 -maxplayers $PLAYERS +map gm_construct -port $PORT
 else
