0cd125cac1ecd65d6db4db56cff0a304f560bdf1
Jonathan Mainguy <jon@soh.re>
2013-10-15 15:09:53 -0400

    Fixed issue where players and ip was not set

--- jar/StarMade.sh
+++ jar/StarMade.sh
@@ -5,6 +5,8 @@ JAVA="$3"
 MEMORY="$4"
 JAR_FILE="$5"
 PORT="$6"
+MAX_PLAYERS="$7"
+IP="$8"
 
 cd $SERVER_DIR/StarMade
 sed -i "s/^MAX_CLIENTS =.*/MAX_CLIENTS = $MAX_PLAYERS/g" $SERVER_DIR/StarMade/server.cfg
