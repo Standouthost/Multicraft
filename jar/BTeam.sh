9cf8467141f79d58cff402135f9855e0419bddcd
Jonathan Mainguy <jon@soh.re>
2014-07-18 12:46:17 -0400

    update what hasnt been updated

---
+++ jar/BTeam.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+SERVER_ID="$1"
+SERVER_DIR="$2"
+SERVER_IP="$3"
+SERVER_PORT="$4"
+MAX_MEMORY="$5"
+START_MEMORY="$6"
+JAVA="$7"
+
+cp "$SERVER_DIR"/server.properties "$SERVER_DIR"/BTeam/
+cd "$SERVER_DIR"/BTeam/
+"$JAVA" -Xmx"$MAX_MEMORY"M -Xms"$START_MEMORY"M -Djline.terminal=jline.UnsupportedTerminal -XX:+UseConcMarkSweepGC -XX:+UseParNewGC -XX:+CMSIncrementalPacing -XX:ParallelGCThreads=4 -XX:+AggressiveOpts -Xincgc -jar "$SERVER_DIR"/BTeam/BTeam.jar nogui
---
+++ jar/pocketmine/bin/php5/include/php/main/build-defs.h
@@ -0,0 +1,91 @@
+/*                                                                -*- C -*-
+   +----------------------------------------------------------------------+
+   | PHP Version 5                                                        |
+   +----------------------------------------------------------------------+
+   | Copyright (c) 1997-2007 The PHP Group                                |
+   +----------------------------------------------------------------------+
+   | This source file is subject to version 3.01 of the PHP license,      |
+   | that is bundled with this package in the file LICENSE, and is        |
+   | available through the world-wide-web at the following url:           |
+   | http://www.php.net/license/3_01.txt                                  |
+   | If you did not receive a copy of the PHP license and are unable to   |
+   | obtain it through the world-wide-web, please send a note to          |
+   | license@php.net so we can mail you a copy immediately.               |
+   +----------------------------------------------------------------------+
+   | Author: Stig Sæther Bakken <ssb@php.net>                             |
+   +----------------------------------------------------------------------+
+*/
+
+/* $Id$ */
+
+#define CONFIGURE_COMMAND " './configure'  '--enable-inline-optimization' '--prefix=/root/bin/php5' '--exec-prefix=/root/bin/php5' '--with-curl=/root/install_data/php/ext/curl' '--with-zlib=/root/install_data/php/ext/zlib' '--with-zlib-dir=/root/install_data/php/ext/zlib' '--with-yaml=/root/install_data/php/ext/yaml' '--without-libedit' '--disable-libxml' '--disable-xml' '--disable-dom' '--disable-simplexml' '--disable-xmlreader' '--disable-xmlwriter' '--disable-cgi' '--disable-session' '--disable-debug' '--disable-phar' '--disable-pdo' '--without-pear' '--without-iconv' '--without-pdo-sqlite' '--enable-ctype' '--enable-sockets' '--enable-shared=no' '--enable-static=yes' '--enable-shmop' '--enable-pcntl' '--enable-pthreads' '--enable-maintainer-zts' '--enable-zend-signals' '--with-mysqli=mysqlnd' '--enable-embedded-mysqli' '--enable-bcmath' '--enable-cli' '--enable-zip' '--with-zend-vm=GOTO'"
+#define PHP_ADA_INCLUDE		""
+#define PHP_ADA_LFLAGS		""
+#define PHP_ADA_LIBS		""
+#define PHP_APACHE_INCLUDE	""
+#define PHP_APACHE_TARGET	""
+#define PHP_FHTTPD_INCLUDE      ""
+#define PHP_FHTTPD_LIB          ""
+#define PHP_FHTTPD_TARGET       ""
+#define PHP_CFLAGS		"$(CFLAGS_CLEAN) -prefer-non-pic -static"
+#define PHP_DBASE_LIB		""
+#define PHP_BUILD_DEBUG		""
+#define PHP_GDBM_INCLUDE	""
+#define PHP_IBASE_INCLUDE	""
+#define PHP_IBASE_LFLAGS	""
+#define PHP_IBASE_LIBS		""
+#define PHP_IFX_INCLUDE		""
+#define PHP_IFX_LFLAGS		""
+#define PHP_IFX_LIBS		""
+#define PHP_INSTALL_IT		""
+#define PHP_IODBC_INCLUDE	""
+#define PHP_IODBC_LFLAGS	""
+#define PHP_IODBC_LIBS		""
+#define PHP_MSQL_INCLUDE	""
+#define PHP_MSQL_LFLAGS		""
+#define PHP_MSQL_LIBS		""
+#define PHP_MYSQL_INCLUDE	""
+#define PHP_MYSQL_LIBS		""
+#define PHP_MYSQL_TYPE		""
+#define PHP_ODBC_INCLUDE	""
+#define PHP_ODBC_LFLAGS		""
+#define PHP_ODBC_LIBS		""
+#define PHP_ODBC_TYPE		""
+#define PHP_OCI8_SHARED_LIBADD 	""
+#define PHP_OCI8_DIR			""
+#define PHP_OCI8_ORACLE_VERSION		""
+#define PHP_ORACLE_SHARED_LIBADD 	"@ORACLE_SHARED_LIBADD@"
+#define PHP_ORACLE_DIR				"@ORACLE_DIR@"
+#define PHP_ORACLE_VERSION			"@ORACLE_VERSION@"
+#define PHP_PGSQL_INCLUDE	""
+#define PHP_PGSQL_LFLAGS	""
+#define PHP_PGSQL_LIBS		""
+#define PHP_PROG_SENDMAIL	""
+#define PHP_SOLID_INCLUDE	""
+#define PHP_SOLID_LIBS		""
+#define PHP_EMPRESS_INCLUDE	""
+#define PHP_EMPRESS_LIBS	""
+#define PHP_SYBASE_INCLUDE	""
+#define PHP_SYBASE_LFLAGS	""
+#define PHP_SYBASE_LIBS		""
+#define PHP_DBM_TYPE		""
+#define PHP_DBM_LIB		""
+#define PHP_LDAP_LFLAGS		""
+#define PHP_LDAP_INCLUDE	""
+#define PHP_LDAP_LIBS		""
+#define PHP_BIRDSTEP_INCLUDE     ""
+#define PHP_BIRDSTEP_LIBS        ""
+#define PEAR_INSTALLDIR         ""
+#define PHP_INCLUDE_PATH	".:"
+#define PHP_EXTENSION_DIR       "/root/bin/php5/lib/php/extensions/no-debug-zts-20121212"
+#define PHP_PREFIX              "/root/bin/php5"
+#define PHP_BINDIR              "/root/bin/php5/bin"
+#define PHP_SBINDIR             "/root/bin/php5/sbin"
+#define PHP_MANDIR              "/root/bin/php5/php/man"
+#define PHP_LIBDIR              "/root/bin/php5/lib/php"
+#define PHP_DATADIR             "${prefix}/share"
+#define PHP_SYSCONFDIR          "/root/bin/php5/etc"
+#define PHP_LOCALSTATEDIR       "/root/bin/php5/var"
+#define PHP_CONFIG_FILE_PATH    "/root/bin/php5/lib"
+#define PHP_CONFIG_FILE_SCAN_DIR    ""
+#define PHP_SHLIB_SUFFIX        "so"
