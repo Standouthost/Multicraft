1cc3cf58ad5a2ab3f726fda31227b9bbed75132c
Jonathan Mainguy <jon@soh.re>
2014-08-15 21:29:32 -0400

    Added libs needed for glibc2.15, fixed some variable names
    Former-commit-id: a480f6311ddc2bdfc382bfaf275a42e194f87339

--- jar/garrysmod.sh
+++ jar/garrysmod.sh
@@ -3,19 +3,19 @@ PLAYERS="$1"
 WORLD="$2"
 PORT="$3"
 SERVER_DIR="$4"
-if [ -d $SERVER_DIR/.steam ]; then
-	echo "cool"
-else
-	mkdir -p "$SERVER_DIR"/.steam/sdk32;
+if [ ! -d ${SERVER_DIR}/.steam ]; then
+	mkdir -p ${SERVER_DIR}/.steam/sdk32;
 fi
 if [ $WORLD == 'update' -o $WORLD == 'world' ]; then
-	cd "$SERVER_DIR"/Steam/
-	"$SERVER_DIR"/Steam/steamcmd.sh +runscript gmod.txt
-	cp "$SERVER_DIR"/Steam/linux32/libsteam.so "$SERVER_DIR"/.steam/sdk32/
-	cp "$SERVER_DIR"/Steam/linux32/libstdc++.so.6 "$SERVER_DIR"/Steam/gmod/bin/
-	cd "$SERVER_DIR"/Steam/gmod
-	"$SERVER_DIR"/Steam/gmod/srcds_run -game garrysmod -autoupdate -tickrate 66 -maxplayers $PLAYERS +map gm_construct -port $PORT
+	cd ${SERVER_DIR}/Steam/
+	${SERVER_DIR}/Steam/steamcmd.sh +runscript gmod.txt
+	cp ${SERVER_DIR}/Steam/linux32/libsteam.so ${SERVER_DIR}/.steam/sdk32/
+	cp ${SERVER_DIR}/Steam/linux32/libstdc++.so.6 ${SERVER_DIR}/Steam/gmod/bin/
+	rsync -avz ${SERVER_DIR}/Steam/lib/ ${SERVER_DIR}/Steam/gmod/bin
+	cd ${SERVER_DIR}/Steam/gmod
+	rsync 
+	${SERVER_DIR}/Steam/gmod/srcds_run -game garrysmod -autoupdate -tickrate 66 -maxplayers $PLAYERS +map gm_construct -port $PORT
 else
-	cd "$SERVER_DIR"/Steam/gmod
-	"$SERVER_DIR"/Steam/gmod/srcds_run -game garrysmod -autoupdate -tickrate 66 -maxplayers $PLAYERS +map $WORLD -port $PORT
+	cd ${SERVER_DIR}/Steam/gmod
+	${SERVER_DIR}/Steam/gmod/srcds_run -game garrysmod -autoupdate -tickrate 66 -maxplayers $PLAYERS +map $WORLD -port $PORT
 fi
